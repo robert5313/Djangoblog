a17f17e16479176acd92de336dc7463325333bd9
robert5313 <robert.kimaiyo@masterclass.co.ke>
2024-02-14 23:50:12 +0300

    fixed some style

--- frontend/src/components/ArticleList.jsx
+++ frontend/src/components/ArticleList.jsx
@@ -22,11 +22,12 @@ const ArticleList = () => {
         <h2>Articles</h2>
         {articles.map(article => (
             <div className='container-card' key={article.id}>
-                <h3><span>Title: {article.title}</span><span> | Views: {article.views}</span> |<span>Category: {article.category}</span></h3>
+                <h3><span>Title: {article.title}</span></h3>
+               <h4> <span> | Views: {article.views}</span> | <span>Category: {article.category}| </span></h4>
                 <p></p>
                 <h3></h3>
-                <h3>Author: {article.author}</h3>
-                <p>Content: {article.content}</p>
+                <h3><span>Author: </span>{article.author}</h3>
+                <p><u>Content: </u>{article.content}</p>
                 <p>Summary: {article.summary}</p>
                 <h>Created_at: {article.created_at}</h>
                 <Link to={`/ArticleList/${article.id}`}>Read More</Link>
