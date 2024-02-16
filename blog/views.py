3ee8b491d7cb85a8fc7f04cfed34b5f4fbcb538a
robert5313 <robert.kimaiyo@masterclass.co.ke>
2024-02-16 23:07:51 +0300

    requirements.txt added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Article
 from .serializers import ArticleSerializer
 from .pagination import CustomPageNumberPagination
@@ -8,6 +8,8 @@ class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     pagination_class = CustomPageNumberPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author', 'category', 'summary', 'content']
 
 class ArticleDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
