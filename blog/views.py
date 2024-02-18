93fb75eaa27fc09ebabcf564905da2f8ea4fe9ba
robert5313 <robert.kimaiyo@masterclass.co.ke>
2024-02-18 09:30:13 +0300

    Added Search filters

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Article
 from .serializers import ArticleSerializer
 from .pagination import CustomPageNumberPagination
@@ -8,9 +9,8 @@ class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     pagination_class = CustomPageNumberPagination
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'author', 'category', 'summary', 'content']
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'title']
 class ArticleDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
--- myblog/settings.py
+++ myblog/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'blog',
     'corsheaders',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
